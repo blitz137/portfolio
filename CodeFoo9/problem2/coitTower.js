1ba81900336d1ef3c56b63b27de42e5025ddae24
blitz137 <43609419+blitz137@users.noreply.github.com>
2019-03-06 22:47:29 -0600

    updates

--- CodeFoo9/problem2/coitTower.js
+++ CodeFoo9/problem2/coitTower.js
@@ -9,16 +9,27 @@ let pokeBall = ballVolume;
 //coit tower height is 210ft tall
 //coit tower area is 74,050 squared feet
 //the volume of coit tower would be equall to area times height
+//assuming the tower is hollowed out to fit the maximum amount of pokeballs
+//the volume will equall the space used to store the pokeballs
 let coitVolume = 74050 * 210;
 let coitTower = coitVolume;
+
+//there will be empty space between each pokeball
+//in geometry sphere packing refers to an arrangement of spheres in a 3 dimensional space.
+//typically if the spheres are equal in size and packed densly there will be 26% of empty space.
+
+//the filled space of the tower is only 74% percent.
 let filledSpace = coitTower * .74;
 
+//the pokeballs are in inches so i need to convert them to feet
 
 let pokeBallFeet = pokeBall /12;
+
+//to find how many pokeballs we divide the volume of the tower that is filled by the volume of a single pokeball
 let answer = filledSpace / pokeBallFeet;
 
 
 console.log(answer);
 
 
-//aprx. 55,680,822 on calc
+//aprx. 55,680,822 pokeballs in the tower on calc
